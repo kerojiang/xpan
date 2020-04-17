65e90d8caad9dddb63f3d6f9cb7f4a854975766a
江杰 <jiangjie27@countrygarden.com.cn>
2020-04-17 13:27:05 +0800

    完成ws前后端对接

--- main.go
+++ main.go
@@ -4,16 +4,33 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func main() {
 
-	xpanWS := Service{}
-	http.HandleFunc("/ws", xpanWS.Do)
-	err := http.ListenAndServe("127.0.0.1:8989", nil)
+	xpanService := NewService()
+
+	// 注册静态文件位置
+	http.Handle("/www/", http.StripPrefix("/www/", http.FileServer(http.Dir("./www/"))))
+
+	// 注册login静态页面
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		t, err := template.ParseFiles("./www/login.html")
+		if err != nil {
+			fmt.Println("启动页面异常:", err)
+		}
+		_ = t.Execute(writer, nil)
+	})
+	// 注册websocket方法
+	http.HandleFunc("/ws", xpanService.Do)
+
+	fmt.Println("请打开浏览器,访问 http://localhost:8989")
+
+	err := http.ListenAndServe(":8989", nil)
 	if err != nil {
 		fmt.Println("启动服务端失败:", err)
 	}
-	fmt.Println("启动服务端成功")
+
 }
--- service.go
+++ service.go
@@ -10,57 +10,100 @@ import (
 )
 
 // 数据Model
-type cmdModel struct {
-	Name string `json:"name"`
-	Cmd  string `json:"cmd"`
+type CmdModel struct {
+	Key string `json:"key"`
+	Cmd string `json:"cmd"`
 }
 
 var (
 	upgrader = websocket.Upgrader{
 		Error: onError,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
 	}
-	conn *websocket.Conn
 )
 
 // service类
 type Service struct {
 }
 
-func (s Service) login(ws *websocket.Conn, msg string) {
-
+// 登录
+func (s Service) login(msg string) string {
+	return ""
 }
 
-func (s Service) logout(ws *websocket.Conn, msg string) {
-
+//
+func (s Service) logout(msg string) string {
+	return ""
 }
 
-// Begin
-func (s Service) Do(w http.ResponseWriter, r *http.Request) {
+// 获取账号列表
+func (s Service) loglist(msg string) string {
+	return ""
+}
 
-	conn, err := upgrader.Upgrade(w, r, nil)
+// 获取用户信息
+func (s Service) who(msg string) string {
+
+	return "123"
+}
+
+// 接收客户端数据
+func (s Service) received(ws *websocket.Conn) {
+	// 读取客户端数据
+	mt, data, err := ws.ReadMessage()
 	if err != nil {
+		panic(err)
+	}
+
+	// 解析返回的字节数组
+	clientCmd := CmdModel{}
+	if err := json.Unmarshal(data, &clientCmd); err != nil {
 		fmt.Println(err)
 	}
 
-	defer conn.Close()
-	// 读取客户端数据
+	// 根据解析到指令执行对应的操作
+	var result string
+	switch clientCmd.Key {
+	case "who":
+		result = s.who(clientCmd.Cmd)
+	case "login":
+		result = s.login(clientCmd.Cmd)
+
+	}
+
+	// 结果返回给前端
+	err = ws.WriteMessage(mt, []byte(result))
+
+	if err != nil {
+		panic(err)
+	}
+
+	s.received(ws)
+}
+
+// 服务端通用方法,解析用户请求,执行对应指令
+func (s Service) Do(w http.ResponseWriter, r *http.Request) {
+	// 初始化
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// 异常处理
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+			ws.Close()
+		}
+	}()
+	s.received(ws)
 
 }
 
-// OnReceive
-func (s Service) OnReceive() (cmdModeld, error) {
-	for {
-		_, data, err := conn.ReadMessage()
-		if err != nil {
-			return cmdModel{}, err
-		}
-
-		// 解析返回的字节数组
-		clientCmd := cmdModel{}
-		if err := json.Unmarshal(data, &clientCmd); err != nil {
-			return clientCmd, nil
-		}
-	}
+// 创建新的service对象
+func NewService() *Service {
+	return &Service{}
 }
 
 // 异常处理
